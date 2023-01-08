f81d604af74f35a2b5273e461a66e139e3f96a02
spselenim <pawal.satish@gmail.com>
2023-01-08 22:20:00 +0530

    ThIRD Master

--- src/test/java/com/stc/jan2023/Third.java
+++ src/test/java/com/stc/jan2023/Third.java
@@ -5,6 +5,6 @@ import org.testng.annotations.Test;
 public class Third {
   @Test
   public void f() {
-	  System.out.println("T H I R D Test");
+	  System.out.println("T H I R D Test M A S T E R1");
   }
 }
