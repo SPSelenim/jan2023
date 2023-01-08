dfabb52cac21bb3747ccf3ba452cef7c5b2bff72
spselenim <pawal.satish@gmail.com>
2023-01-08 21:25:30 +0530

    First

---
+++ src/test/java/com/stc/jan2023/FirstTest.java
@@ -0,0 +1,12 @@
+package com.stc.jan2023;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+  @Test
+  public void f() {
+  System.out.println("First TesntNG");
+  }
+  
+  
+}
---
+++ src/test/java/com/stc/jan2023/Launchbrowser.java
@@ -0,0 +1,34 @@
+package com.stc.jan2023;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Launchbrowser {
+  @Test
+  public void f() {
+	  System.out.println("***************L A U N C H B R O W S E R **********************");
+ try
+ {
+	 
+ WebDriver driver=null;
+		WebDriverManager.chromedriver().setup();
+		 driver = new ChromeDriver();
+			driver.get("E:\\SeleniumTraining1\\TestingWorld.html");
+			driver.manage().window().maximize();
+			driver.findElement(By.partialLinkText("link")).click();
+			String aa = driver.findElement(By.xpath("//label")).getText();
+			System.out.println(aa);
+			  System.out.println("***************C L O S E B R O W S E R **********************");
+ driver.quit();
+ }
+ 
+ catch(Exception e)
+ {
+	 System.out.println("ERROR ::::"+e.getMessage());
+ }
+  }
+}
